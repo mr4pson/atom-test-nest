8fbf3bb8b8211da1c553def8e35289e72a2d4d68
Evgenii Goriachkin <Evgenii.Goriachkin@payever.org>
2021-03-17 07:04:58 +0300

    added authentication and authorization

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ ignoreEnvFile: true }), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ ignoreEnvFile: true }),
+    DatabaseModule,
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
