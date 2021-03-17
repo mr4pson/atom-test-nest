a176a45e041d8942fbffcf7b5bedc556c024b6d1
Evgenii Goriachkin <Evgenii.Goriachkin@payever.org>
2021-03-17 06:27:07 +0300

    added database and configs

---
+++ src/database/database-models.providers.ts
@@ -0,0 +1,11 @@
+import { Connection } from 'mongoose';
+import { DATABASE_CONNECTION, USER_MODEL } from './database.constants';
+import { userModelFn } from './user.model';
+
+export const databaseModelsProviders = [
+  {
+    provide: USER_MODEL,
+    useFactory: (connection: Connection) => userModelFn(connection),
+    inject: [DATABASE_CONNECTION],
+  },
+];
